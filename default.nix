8793c8f4d52033861cbf614783bc3d9e772df979
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-16 13:03:04 +0000

    Move shell.nix to default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,4 @@
 let
   np = import <nixpkgs> {};
-  ghc = np.callPackage ./artifact.nix {};
-
-in
-  ghc
+  ghc = np.callPackage ./artifact.nix {} {};
+in np.mkShell { buildInputs = [ghc np.haskellPackages.cabal-install ]; }
--- shell.nix
+++
@@ -1,4 +0,0 @@
-let
-  np = import <nixpkgs> {};
-  ghc = np.callPackage ./artifact.nix {} {};
-in np.mkShell { buildInputs = [ghc np.haskellPackages.cabal-install ]; }
