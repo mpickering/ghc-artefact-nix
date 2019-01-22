cb9d39a311546e7089482c514429a9e45d87e6bd
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-22 16:25:06 +0000

    Fix syntax error

--- ghc-head-from.nix
+++ ghc-head-from.nix
@@ -3,4 +3,4 @@
 let scr = writeScriptBin "ghc-head-from" ''${./ghc-artefact-nix/ghc-head-from.sh} "$@"'';
 in runCommand "ghc-head-from" { buildInputs = [makeWrapper scr]; }
 					''makeWrapper ${scr}/bin/ghc-head-from $out/bin/ghc-head-from \
-						--prefix PATH : ${makeBinPath [jq]}'';
+						--prefix PATH : ${makeBinPath [jq]}''
