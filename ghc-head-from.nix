3a05c240147d1425b0d05ffa52e400454cfae593
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-22 16:27:02 +0000

    Fix many things wrong

--- ghc-head-from.nix
+++ ghc-head-from.nix
@@ -1,6 +1,6 @@
 # Wraps ghc-head-from.sh to provide the jq dependency
-{ writeScriptBin, lib, jq, runCommand, makWrapper }:
-let scr = writeScriptBin "ghc-head-from" ''${./ghc-artefact-nix/ghc-head-from.sh} "$@"'';
+{ writeScriptBin, lib, jq, runCommand, makeWrapper }:
+let scr = writeScriptBin "ghc-head-from" ''${./ghc-head-from.sh} "$@"'';
 in runCommand "ghc-head-from" { buildInputs = [makeWrapper scr]; }
 					''makeWrapper ${scr}/bin/ghc-head-from $out/bin/ghc-head-from \
-						--prefix PATH : ${makeBinPath [jq]}''
+						--prefix PATH : ${lib.makeBinPath [jq]}''
