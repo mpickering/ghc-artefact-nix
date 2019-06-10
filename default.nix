901ddf0c38a94cceb9e4f600eafddc5330ed0b7a
Matthew Pickering <matthewtpickering@gmail.com>
2019-06-10 13:50:43 +0100

    Support passing a URL directly

--- default.nix
+++ default.nix
@@ -1,13 +1,19 @@
-{ branch ? "master", fork ? "ghc" }:
+{ branch ? "master", fork ? "ghc", url ? null, ncursesVersion ? "6" }:
 let
   np = import <nixpkgs> {};
   ghc = self: ref: self.callPackage ./artifact.nix {} ref;
+
+  gitlabConfig = self: (self.callPackage ./gitlab-artifact.nix {} {
+                    inherit fork branch;
+                  });
+  directConfig = { bindistTarball = builtins.fetchurl url;
+                   inherit ncursesVersion; };
+
+  config = self: if url == null then (gitlabConfig self) else directConfig;
   ol = self: super:
     {
       ghcHEAD =
-        ghc self (self.callPackage ./gitlab-artifact.nix {} {
-          inherit fork branch;
-        });
+        ghc self (config self) ;
     };
 in
   import <nixpkgs> { overlays = [ol]; }
--- ghc-head-from.sh
+++ ghc-head-from.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-if ! [ -z "$1" ];
+re='^[0-9]+$'
+if [[ $1 =~ $re ]] ;
 then
 	# If a MR number is passed then lookup which branch to get artefacts from.
   REQ=$(curl -s https://gitlab.haskell.org/api/v4/projects/1/merge_requests/$1)
@@ -9,6 +10,13 @@ then
 	PARSED_FORK=$(echo $PROJ_REQ | jq -r '.namespace.path')
 	MR_TITLE=$(echo $REQ | jq -r '.title')
 	echo "Fetching from MR: $MR_TITLE"
+elif ! [ -z "$1" ]; then
+  # Assume the argument is a URL
+  echo "Fetching artefact from $1"
+  nix run -f . \
+    --argstr url $1 \
+    ghcHEAD cabal-install gcc binutils-unwrapped
+  exit 0
 fi
 # Set the default values if there was no parameter
 FORK=''${PARSED_FORK:-ghc}
